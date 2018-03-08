ee95a4e6338a8848a875780770d753f7f03c5987
PrinzEugen <nsi05162@pooae.com>
2018-03-08 18:09:12 +0900

    Update RobotController.cs

--- Simulation/Robot/Wheel4/WheelRobot1/Assets/Scripts/RobotController.cs
+++ Simulation/Robot/Wheel4/WheelRobot1/Assets/Scripts/RobotController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Networking.Types;
@@ -15,27 +15,26 @@ public class AxleInfo
 public class RobotController : MonoBehaviour
 {
 	public List<AxleInfo> axleInfos;
-	public float MaxMotorTorque = 400;
-	public float MaxSteeringAngle = 30;
+	public float MaxMotorTorque = 400;  // モーターの最大トルク
+	public float MaxSteeringAngle = 30; // ステアリングの最大回転角度
 
 	public void ApplyLocalPosiitonToVisuals (WheelCollider collider)
 	{
-		// Wheelコライダーの子要素がない場合は処理終了。
+		// エラー処理（車輪用コライダーの子要素がなければ終了）
 		if (collider.transform.childCount == 0)
 			return;
 
-		// タイヤビジュアルを取得。
-		Transform visual = collider.transform.GetChild (0);
+		// 車輪ビジュアルの子要素を取得
+		Transform wheel = collider.transform.GetChild (0);
 
-		// コライダーの位置と回転を取得。
+		// コライダーの位置・回転角を取得
 		Vector3 pos;
 		Quaternion q;
 		collider.GetWorldPose (out pos, out q);
 
-		// タイヤビジュアルに、コライダーの値を設定。
-		// 単純な車の場合、Z軸を90f回転させる必要がある
-		visual.transform.position = pos;
-		visual.transform.rotation = q * Quaternion.Euler (0f, 0f, 90f);
+		// 車輪ビジュアルの位置と回転角を設定
+		wheel.transform.position = pos;
+		wheel.transform.rotation = q * Quaternion.Euler (0f, 0f, 90f);
 	}
 
 	void FixedUpdate ()
